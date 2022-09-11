2758e005c35d8859a7dec1b2dd188fbb9832ba1f
Abhinav Badireddy <87709968+abhinavbadireddy@users.noreply.github.com>
2022-09-11 18:45:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function currentTime() {
+    var date = new Date(); 
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; 
+      var t = setTimeout(function(){ currentTime() }, 1000); 
+}
+  
+function updateTime(k) {
+    if (k < 10) {
+      return "0" + k;
+    }
+    else {
+      return k;
+    }
+}
+  
+currentTime(); 
+
+var name = prompt("Please enter your name: ");
+    if (name != null) {
+      document.getElementById("welcome").innerText = name + "!";
+    }
+
+if (name.value === "") {
+    alert("Please enter a name")
+}
+
+document.getElementById("modify").innerHTML = document.lastModified
